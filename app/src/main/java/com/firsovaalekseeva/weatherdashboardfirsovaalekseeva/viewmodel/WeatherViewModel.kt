6ed4432188111c0efc137bed457fe03395bbc225
dazzes4545 <dazzes7619@gmail.com>
2026-04-08 09:28:15 +0300

    Step 5: Create ViewModel with sequential loading

---
+++ app/src/main/java/com/firsovaalekseeva/weatherdashboardfirsovaalekseeva/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,43 @@
+package com.firsovaalekseeva.weatherdashboardfirsovaalekseeva.viewmodel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.firsovaalekseeva.weatherdashboardfirsovaalekseeva.data.WeatherData
+import com.firsovaalekseeva.weatherdashboardfirsovaalekseeva.data.WeatherRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class WeatherViewModel: ViewModel()  {
+    private val repository=WeatherRepository()
+    private val _weatherState = MutableStateFlow(WeatherData())
+    val weatherState: StateFlow<WeatherData> = _weatherState.asStateFlow()
+    init{
+        loadWeatherData()
+    }
+    fun loadWeatherData(){
+        viewModelScope.launch {
+            _weatherState.value = _weatherState.value.copy(
+                isLoading = true,
+                error = null
+            )
+            try {
+                val temperature = repository.fetchTemperature()
+                _weatherState.value = _weatherState.value.copy(temperature = temperature)
+                val humidity = repository.fetchHumidity()
+                _weatherState.value = _weatherState.value.copy(humidity = humidity)
+                val windSpeed = repository.fetchWindSpeed()
+                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
+
+                _weatherState.value = _weatherState.value.copy(isLoading = false)
+            }catch (e: Exception){
+                _weatherState.value=_weatherState.value.copy(
+                    isLoading = false,
+                    error = "Ошибка загрузки: ${e.message}"
+                )
+            }
+        }
+    }
+
+}
