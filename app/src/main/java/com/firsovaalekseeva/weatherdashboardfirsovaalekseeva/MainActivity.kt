53dcc7a6fb17bab57fd1f74257df5718786af371
dazzes4545 <dazzes7619@gmail.com>
2026-04-08 08:51:39 +0300

    Initial commit

---
+++ app/src/main/java/com/firsovaalekseeva/weatherdashboardfirsovaalekseeva/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.firsovaalekseeva.weatherdashboardfirsovaalekseeva
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.firsovaalekseeva.weatherdashboardfirsovaalekseeva.ui.theme.WeatherDashboardFirsovaAlekseevaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherDashboardFirsovaAlekseevaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherDashboardFirsovaAlekseevaTheme {
+        Greeting("Android")
+    }
+}
